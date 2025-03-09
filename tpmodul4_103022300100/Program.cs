d233c48a61e5764dca7808f7b0b27c17f54543b7
Irfan Rangga <irfanrangga04@gmail.com>
2025-03-09 16:42:23 +0700

    menambahkan implementasi dengan table-driven

--- tpmodul4_103022300100/Program.cs
+++ tpmodul4_103022300100/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class MainClass
+{
+    public static void Main()
+    {
+        Console.WriteLine(KodePos.getKodePos(KodePos.Kelurahan.Margasari));
+    }
+}
+
+public class KodePos
+{
+    public enum Kelurahan { Batununnggal, Kujangsari, Mengger, Wates, Cijaura, 
+        Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja}
+
+    public static string getKodePos(Kelurahan kelurahan)
+    {
+        string[] kodePos = { "40266", "40287", "40267", "40256", "40287",
+            "40286", "40286", "40286", "40272", "40274", "40273"};
+
+        return kodePos[(int)kelurahan];
+    }
+}
